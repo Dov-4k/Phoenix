b6025e9535ca19d22f38c02839c3a3f2518547c7
Dov-4k <dovabrahimkanner@gmail.com>
2023-02-26 14:47:06 -0500

    First commit

---
+++ Phoenix/screen.py
@@ -0,0 +1,15 @@
+from cmu_112_graphics import *
+
+def onAppStart(app):
+    pass
+
+def onMousePress(app, mouseX, mouseY):
+    pass
+
+def redrawAll(app):
+    pass
+
+def main():
+    runApp()
+
+main()
