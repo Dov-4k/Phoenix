a577b473f26263543446bc20ed72095a1e7f9178
Dov-4k <dovabrahimk@gmail.com>
2023-03-24 10:17:49 -0400

    Fourth commit

--- Phoenix/phoenix.py
+++ Phoenix/phoenix.py
@@ -16,55 +16,49 @@ def onAppStop(app):
 
 def startScreen_onAppStart(app):
     print('In startScreen_onAppStart')
-    app.color = 'gold'
-    app.activatedCounter1 = 0
 
 def startScreen_onScreenActivate(app):
     print('In startScreen_onScreenActivate')
-    app.activatedCounter1 += 1
 
-def startScreen_onKeyPress(app, key):
-    if key == 's': setActiveScreen('screen2')
-    elif key == 'c': app.color = 'navy' if (app.color == 'gold') else 'gold'
+def startScreen_onMousePress(app, x, y):
+    if x >= 130 and x <= 180 and y >= 44 and y <= 55: setActiveScreen('game')
 
 def startScreen_redrawAll(app):
-    drawLabel('Screen 1', app.width/2, 30, size=16)
-    drawLabel('Press c to change square color', app.width/2, 50, size=16)
-    drawLabel('Press s to change screen to screen2', app.width/2, 70, size=16)
+    drawRect(0, 0, app.width, app.height, fill='black')
+    drawLabel('Welcome to Phoenix 3', app.width/2, 30, size=16, fill='white')
+    drawLabel('Press START to begin the game', app.width/2, 50, size=16, fill='white')
     drawRect(100, 100, app.width-200, app.height-200, fill=app.color)
-    drawLabel(f'Screen1 was activated {app.activatedCounter1} times',
-            app.width/2,app.height-30, size=12)
 
 ##################################
-# Screen2
+# Game
 ##################################
 
-def screen2_onAppStart(app):
-    print('In screen2_onAppStart')
+def game_onAppStart(app):
+    print('In game')
     app.cx = app.width/2
     app.dx = 10
 
-def screen2_onScreenActivate(app):
-    print('In screen2_onScreenActivate')
+def game_onScreenActivate(app):
+    print('In game_onScreenActivate')
 
-def screen2_onKeyPress(app, key):
-    if key == 's': setActiveScreen('screen1')
+def game_onKeyPress(app, key):
+    if key == 's': setActiveScreen('startScreen')
     elif key == 'd': app.dx = -app.dx
 
-def screen2_onStep(app):
+def game_onStep(app):
     app.cx = (app.cx + app.dx) % app.width
 
-def screen2_redrawAll(app):
-    drawLabel('Screen 2', app.width/2, 30, size=16)
+def game_redrawAll(app):
+    drawLabel('Game', app.width/2, 30, size=16)
     drawLabel('Press d to change direction of dot', app.width/2, 50, size=16)
-    drawLabel('Press s to change the screen to screen1', app.width/2, 70, size=16)
+    drawLabel('Press s to change the screen to startScreen', app.width/2, 70, size=16)
     drawCircle(app.cx, app.height/2, 50, fill='lightGreen')
 
-##################################
+##################################c
 # main
 ##################################
 
 def main():
-    runAppWithScreens(initialScreen='screen1', width=800)
+    runAppWithScreens(initialScreen='startScreen', height=700)
 
 main()
